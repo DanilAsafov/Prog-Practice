5de3b49a65ec051424f951c227614b8d797477ec
danilasafov <perez3498145@gmail.com>
2025-04-10 15:19:34 +0600

    fix: style change

--- LW-1/Problem-5.c
+++ LW-1/Problem-5.c
@@ -9,11 +9,9 @@ double trapeziodal_integration(double (*f)(double), double a, double b, int n) {
   if (n < 0) {
     return UNPOSITIVE_NUMBER;
   }
-  
   if (a > b) {
     return REVERSED_LIMITS;
   }
-
   double h = (b - a) / n;
   double sum = 0.5 * (fabs(f(a)) + fabs(f(b)));
   for (int i = 1; i < n; i++) {
